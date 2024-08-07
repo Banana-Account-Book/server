4a912277b753571d140fd17e4ec15b58dbc4401e
changchanghwang <hch950627@naver.com>
2024-08-07 22:43:13 +0900

    fix: model type

--- internal/services/users/domain/model.go
+++ internal/services/users/domain/model.go
@@ -1,15 +1,18 @@
 package domain
 
-import "banana-account-book.com/internal/libs/entity"
+import (
+	"banana-account-book.com/internal/libs/entity"
+	"github.com/google/uuid"
+)
 
 type User struct {
 	entity.SoftDeletableAggregate
-	Id           string   `json:"id" gorm:"primaryKey; type:varchar(16)"`
-	Email        string   `json:"email" gorm:"unique;type:varchar(50)"`
-	Password     string   `json:"password" gorm:"type:varchar(255)"`
-	Name         string   `json:"name" gorm:"type:varchar(50)"`
-	Providers    []string `json:"providers" gorm:"type:jsonb"`
-	RefreshToken string   `json:"refreshToken" gorm:"column:refreshToken;type:varchar(255)"`
+	Id           uuid.UUID `json:"id" gorm:"primaryKey; type:uuid"`
+	Email        string    `json:"email" gorm:"unique;type:varchar(50)"`
+	Password     string    `json:"password" gorm:"type:varchar(255)"`
+	Name         string    `json:"name" gorm:"type:varchar(50)"`
+	Providers    []string  `json:"providers" gorm:"type:text[]"`
+	RefreshToken string    `json:"refreshToken" gorm:"column:refreshToken;type:varchar(255)"`
 }
 
 func (u *User) TableName() string {
